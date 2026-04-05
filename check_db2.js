87ea284de54c1e07b5abd51283c5f2b16503acc9
thulinh2005 <dothithulinh2005@gmail.com>
2026-04-06 01:50:45 +0700

    xóa file rác

--- check_db2.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config({ path: "./backend/.env" });
-const db = require("./backend/src/config/db");
-
-db.query("SELECT hd.ma_hd, hd.ma_nv, nv.ho_ten as ten_nv FROM hoa_don hd LEFT JOIN nhan_vien nv ON hd.ma_nv = nv.ma_nv", (err, result) => {
-  if (err) console.error(err);
-  else console.log(result);
-  process.exit();
-});
